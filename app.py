db1f0ba6cd7387942bb62d67a406370c91c8c887
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-06-13 16:35:55 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# use Variables without let or const
+# print is like using console.log for JS
+# input lets you use data you put on the terminal that is stored for use later
+# int converts strings to intergers, float is like int but lets you use decimals
+# 
+
+# name = input("what is your name? ")
+# print("Hello " + name)
+
+first = input("First: ")
+second = input("Second: ")
+sum = int(first) + float(second)
+print("Sum: " + str(sum))
